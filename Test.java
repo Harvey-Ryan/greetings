6eedfbecd9dea3eee3b63080e747ec9d6512da13
Harvey-Ryan <124230161+Harvey-Ryan@users.noreply.github.com>
2023-08-02 18:54:40 -0500

    First java

---
+++ Test.java
@@ -0,0 +1,12 @@
+// class name must match file name
+public class Test {
+    //public static void are essentially the same as Function in JS
+    //main is the name of the method
+    //Parenthesis dictate data
+    public static void main(String[] args) {
+        //System.out.println is the same as console.log
+        System.out.println("My name is Ryan Harvey");
+        System.out.println("I am 33 years old");
+        System.out.println("My hometown is Joplin, MO");
+    }
+}
